575a160252405c4e45c73be519435a17588dcd6b
chuluq <chuluq@arvis.id>
2024-10-26 09:10:41 +0700

    feat: add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "@/assets/logo-with-text.png";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto max-w-screen-xl font-sans">
+      <header className="flex items-center justify-between pt-6">
+        <Link href="/">
+          <Image src={logo} alt="creative-agency-logo" />
+        </Link>
+        <nav>
+          <ul className="flex items-center gap-x-8">
+            <li>
+              <Link
+                href="#pricing"
+                className="font-medium capitalize text-[#0F2137]"
+              >
+                pricing
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#support"
+                className="font-medium capitalize text-[#0F2137]"
+              >
+                support
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#register"
+                className="font-medium capitalize text-[#0F2137]"
+              >
+                register
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#get-started"
+                className="font-bold capitalize text-[#F94962] underline underline-offset-8"
+              >
+                get started
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
